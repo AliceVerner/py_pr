5cca8adbcb66201ac4259c7e1ab74ed5a561d855
AliceVerner <aliceverner@yandex.ru>
2017-05-22 11:57:11 +0300

    refactoring

--- main.py
+++ main.py
@@ -14,18 +14,14 @@ def openTextFile():
 def closeApplication():
         root.destroy()
 
-def aboutWindow():
-    kek = messagebox.showinfo("about", "Made with love in RSSU, 2017")
+def openAboutWindow():
+    aboutWindow = messagebox.showinfo("about", "Made with love in RSSU, 2017")
 
 def countNumberOfSentences(text):
     return text.count('.')+text.count('!')+text.count('?')
  
 def updateCounter(event):              #обработчик событий 
-    textForCounting = textArea.get("1.0",END)
-    sentenceCount = countNumberOfSentences(textForCounting)
-    sentenceCount = str(sentenceCount)
-    outputText = "Введите текст в поле\n" + "В тексте " + sentenceCount + " предложений"
-    textCount.configure(text=outputText)
+    updateCounterForDownloadedFile()
 
 def updateCounterForDownloadedFile():              #обработчик событий 
     textForCounting = textArea.get("1.0",END)
@@ -42,7 +38,7 @@ root.config(menu=mainMenu)
 mainMenuLine = Menu(mainMenu)
 mainMenu.add_cascade(label="Options",menu=mainMenuLine)
 mainMenuLine.add_command(label="Open file",command=openTextFile)
-mainMenuLine.add_command(label="About", command=aboutWindow)
+mainMenuLine.add_command(label="About", command=openAboutWindow)
 mainMenuLine.add_command(label="Exit", command=closeApplication)
  
 textCount = Label(root,width=60, font="14",        #плашка сверху
