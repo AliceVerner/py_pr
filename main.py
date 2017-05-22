d428a0fa08ad8ec3bd6e3c0856e8a163c2a39490
AliceVerner <aliceverner@yandex.ru>
2017-05-22 10:29:01 +0300

    Add downloading files, about window, exit button and interactive count of sentences

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
 #!/usr/bin/env python
 
 from tkinter import *
+from tkinter.filedialog import *
+from tkinter import messagebox
+import fileinput
+
+def openTextFile():
+     TextFileData = askopenfilename()
+     for l in fileinput.input(TextFileData):
+          textArea.insert(END,l)
+     updateCounterForDownloadedFile()
+
+def closeApplication():
+        root.destroy()
+
+def aboutWindow():
+    kek = messagebox.showinfo("about", "Made with love in RSSU, 2017")
 
 def countNumberOfSentences(text):
-    return text.count('. ')+text.count('! ')+text.count('? ')
+    return text.count('.')+text.count('!')+text.count('?')
  
 def updateCounter(event):              #обработчик событий 
     textForCounting = textArea.get("1.0",END)
     sentenceCount = countNumberOfSentences(textForCounting)
     sentenceCount = str(sentenceCount)
-    outputText = "Введите текст в поле и нажмите Enter\n" + "В тексте: " + sentenceCount + "предложений."
+    outputText = "Введите текст в поле\n" + "В тексте " + sentenceCount + " предложений"
     textCount.configure(text=outputText)
+
+def updateCounterForDownloadedFile():              #обработчик событий 
+    textForCounting = textArea.get("1.0",END)
+    sentenceCount = countNumberOfSentences(textForCounting)
+    sentenceCount = str(sentenceCount)
+    outputText = "Введите текст в поле\n" + "В тексте " + sentenceCount + " предложений"
+    textCount.configure(text=outputText)    
  
 root = Tk()
+root.title("Счётчик предложений")
+
+mainMenu = Menu(root)
+root.config(menu=mainMenu)
+mainMenuLine = Menu(mainMenu)
+mainMenu.add_cascade(label="Options",menu=mainMenuLine)
+mainMenuLine.add_command(label="Open file",command=openTextFile)
+mainMenuLine.add_command(label="About", command=aboutWindow)
+mainMenuLine.add_command(label="Exit", command=closeApplication)
  
 textCount = Label(root,width=60, font="14",        #плашка сверху
-            text="Введите текст в поле и нажмите Enter")
+            text="Введите текст в поле\nВ тексте 0 предложений")
 
 scry = Scrollbar( root, orient=VERTICAL )   #скроллбар
 scry.pack( side = RIGHT, fill=Y )
@@ -28,5 +59,6 @@ textCount.pack()
 textArea.pack( side = LEFT, fill = BOTH, expand = True )
 scry.config( command = textArea.yview )
 
-textArea.bind("<Return>",updateCounter)
+textArea.bind("<KeyPress>",updateCounter)
+
 root.mainloop()
